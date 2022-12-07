ca59fba9dd1d96a2ddfc8f40c5a95504a008d7d9
Hamza Raza <hamzaraza77094@gmail.com>
2022-12-07 15:40:59 -0600

    added average for search option

--- search.php
+++ search.php
@@ -8,9 +8,18 @@ include("db.php");
 
 if (isset($_POST['submit'])) {
   $str = mysqli_real_escape_string($conn, $_POST['str']);
+  // $sql = "SELECT tutor_classes.NAME AS 'CLASS_NAME', tutor_classes.CLASS_CODE, tutor_classes.CLASS_NUMBER, 
+  // tutor_classes.TUTOR_ID, tutor_classes.CLASS_ID, tutor_subjects.* DSQ
+  // FROM `tutor_subjects`, `tutor_classes`
+  
+  // WHERE tutor_classes.NAME LIKE '%$str%'
+  // OR tutor_classes.CLASS_CODE LIKE '%$str%'
+  // OR tutor_classes.CLASS_NUMBER LIKE'%$str%'
+  // OR tutor_subjects.NAME LIKE '%$str%';";
+  
   $sql = "SELECT tutor_classes.NAME AS 'CLASS_NAME', tutor_classes.CLASS_CODE, tutor_classes.CLASS_NUMBER, 
-  tutor_classes.TUTOR_ID, tutor_classes.CLASS_ID, tutor_subjects.* 
-  FROM `tutor_subjects`, `tutor_classes`
+  tutor_classes.TUTOR_ID, tutor_classes.CLASS_ID, tutor_subjects.*, tutor.AVG_RATING
+  FROM `tutor_subjects`, `tutor_classes`, tutor
   
   WHERE tutor_classes.NAME LIKE '%$str%'
   OR tutor_classes.CLASS_CODE LIKE '%$str%'
@@ -22,7 +31,7 @@ if (isset($_POST['submit'])) {
   if ($result->num_rows > 0) {
     while($row = ($result->fetch_assoc())) {
       echo "<br>Name: " . $row['F_NAME'] . " " .$row['L_NAME'] ."<br>Class Code: ". $row['CLASS_CODE'] . "<br>Class Number: " . $row['CLASS_NUMBER'] 
-      . "<br>Email: " . $row['EMAIL'] . "<br>Classes: ". $row['CLASS_NAME'] . "<br>Subjects: ". $row['NAME']. "<br>";
+      . "<br>Email: " . $row['EMAIL'] . "<br>Classes: ". $row['CLASS_NAME'] . "<br>Subjects: ". $row['NAME'].  "<br>Average Rating: " . $row['AVG_RATING'] . "<br>";
     }
   } else {
     echo "<br>0 results";
